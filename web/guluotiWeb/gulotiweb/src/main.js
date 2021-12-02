c91424a01fa38e32a04b05fb80c6bbd0a1fa0808
xf1946499535 <863983969@qq.com>
2021-12-02 14:53:22 +0800

    项目基础组件初始化

--- web/guluotiWeb/gulotiweb/src/main.js
+++ web/guluotiWeb/gulotiweb/src/main.js
@@ -2,9 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'element-ui/lib/theme-chalk/index.css';
+import '@/styles/element-variables.scss'
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 引入样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import VueParticles from 'vue-particles'
 
+Vue.use(VueParticles)
+Vue.use(VueQuillEditor)
 Vue.config.productionTip = false
-
+Vue.use(ElementUI);
 new Vue({
   router,
   store,
